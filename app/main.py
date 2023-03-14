374ae67ded9ceb330bcc2a988813a9df43452845
Sasha Semenenko <semenenko233249@gmail.com>
2023-03-14 20:17:07 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    with open(data_file_name, "r") as new_data_file_name:
+        supply = 0
+        buy = 0
+        for word in new_data_file_name:
+            word_ = word.split(",")
+            if word_[0] == "supply":
+                supply += int(word_[1])
+            if word_[0] == "buy":
+                buy += int(word_[1])
+    with open(report_file_name, "w") as upgrade_report_file_name:
+        upgrade_report_file_name.write(f"supply,{supply}\n"
+                                       f"buy,{buy}\n"
+                                       f"result,{supply - buy}\n")
